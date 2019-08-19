2cb395d93a85dbe44cd0c9ef7bdedb6bbd766c29
github username <github email address>
2019-08-19 15:03:04 -0500

    Refinements

--- bin/run.rb
+++ bin/run.rb
@@ -2,4 +2,16 @@ require_relative "../lib/api_communicator.rb"
 require_relative "../config/environment.rb"
 # require_relative "../lib/command_line_interface.rb"
 
-get_events_from_api
+def getZip
+    puts "Greetings! Please enter your Zip Code to find events! or type Exit to quit the program."
+    zip = gets.chomp
+    if zip.downcase == "exit"
+        return
+    else 
+        get_events_from_api(zip)
+        getZip
+    end
+end
+
+# get_events_from_api(zip)
+getZip
--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -1,7 +1,7 @@
-def get_events_from_api()
+def get_events_from_api(zipCode)
     #make the web request
     # response_string = RestClient.get('https://app.ticketmaster.com/discovery/v1/events.json?dmaId=300&apikey=37zNi3JGSm5xKVWdPVAKsUJB7cfQVoyg')
-    response_string = RestClient.get('https://app.ticketmaster.com/discovery/v2/events.json?postalCode=77005&radius=15&unit=miles&apikey=37zNi3JGSm5xKVWdPVAKsUJB7cfQVoyg')
+    response_string = RestClient.get("https://app.ticketmaster.com/discovery/v2/events.json?postalCode=#{zipCode}&radius=15&unit=miles&apikey=37zNi3JGSm5xKVWdPVAKsUJB7cfQVoyg")
     response_hash = JSON.parse(response_string)
 
     puts response_hash
