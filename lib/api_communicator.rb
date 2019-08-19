f6df0026d02d4d79641c10b324fa160725320085
github username <github email address>
2019-08-19 18:01:08 -0500

    basic search logic achieved

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,7 +4,28 @@ def get_events_from_api(zipCode)
     response_string = RestClient.get("https://app.ticketmaster.com/discovery/v2/events.json?postalCode=#{zipCode}&radius=15&unit=miles&apikey=37zNi3JGSm5xKVWdPVAKsUJB7cfQVoyg")
     response_hash = JSON.parse(response_string)
 
-    puts response_hash
+
+     nameArray = []
+     eventArray = response_hash["_embedded"]["events"].map
+    #  nameArray = eventArray["_embedded"]["venues"].map
+    
+     eventArray.each do |event|
+        # container1 = ""
+        # container2 = ""
+        # puts
+        print event["name"] + "             "
+        # puts
+        event["_embedded"]["venues"].each do |venue|
+            puts "@" + venue["name"]
+        end
+     end
+
+    #  nameArray.each do |venue|
+    #     puts venue
+    #  end
+
+
+    # puts response_hash["_embedded"]["events"][0]["_embedded"]["venues"][0]["name"]
     
     # resultsArray = response_hash.fetch("results")
     # films = []
